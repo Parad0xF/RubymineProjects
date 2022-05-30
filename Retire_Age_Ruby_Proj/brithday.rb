3acd61d50abbac29da3ed55b9f575d80e64c0264
radu.enachi@gmail.com <radu.enachi@gmail.com>
2022-05-30 12:04:13 -0700

    initial commit

---
+++ Retire_Age_Ruby_Proj/brithday.rb
@@ -0,0 +1,31 @@
+﻿#This project was made by Radu Enachi
+# 5/30/2022
+# CSIS 280 Professor Tony B.
+
+class Birthday
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def happy_birthday
+    puts "happy Birthday #{@name}, Age: #{@age}."
+  end
+
+  def retire_age
+    puts "There are #{65-@age} years for your retirement."
+  end
+
+  def set_age(age)
+    @age = age
+  end
+end
+
+# Creating object
+bday = Birthday.new("Radu", 32)
+# Calling methods
+bday.happy_birthday
+bday.retire_age
+bday.set_age(45)
+bday.happy_birthday
+bday.retire_age
